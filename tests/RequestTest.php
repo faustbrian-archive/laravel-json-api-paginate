72381563baab1d5c9e8495f8d09ae761d9c4dede
Brian Faust <hello@basecode.sh>
2021-10-09 10:02:06 +0300

    wip

--- tests/RequestTest.php
+++ tests/RequestTest.php
@@ -2,36 +2,24 @@
 
 declare(strict_types=1);
 
-it('will discover the page size parameter', function (): void {
-    $response = $this->get('/?page[size]=2');
+it('will discover the page size parameter')
+    ->get('/?page[size]=2')
+    ->assertJsonFragment(['per_page' => 2]);
 
-    $response->assertJsonFragment(['per_page' => 2]);
-});
+it('will discover the page number parameter')
+    ->get('/?page[number]=2')
+    ->assertJsonFragment(['current_page' => 2]);
 
-it('will discover the page number parameter', function (): void {
-    $response = $this->get('/?page[number]=2');
+it('will use the default page size')
+    ->get('/')
+    ->assertJsonFragment(['per_page' => 30]);
 
-    $response->assertJsonFragment(['current_page' => 2]);
-});
+it('will use the configured page size parameter')
+    ->config(['json-api-paginate.size_parameter' => 'modified_size'])
+    ->get('/?page[modified_size]=2')
+    ->assertJsonFragment(['per_page' => 2]);
 
-it('will use the default page size', function (): void {
-    $response = $this->get('/');
-
-    $response->assertJsonFragment(['per_page' => 30]);
-});
-
-it('will use the configured page size parameter', function (): void {
-    config(['json-api-paginate.size_parameter' => 'modified_size']);
-
-    $response = $this->get('/?page[modified_size]=2');
-
-    $response->assertJsonFragment(['per_page' => 2]);
-});
-
-it('will use the configured page number parameter', function (): void {
-    config(['json-api-paginate.number_parameter' => 'modified_number']);
-
-    $response = $this->get('/?page[modified_number]=2');
-
-    $response->assertJsonFragment(['current_page' => 2]);
-});
+it('will use the configured page number parameter')
+    ->config(['json-api-paginate.number_parameter' => 'modified_number'])
+    ->get('/?page[modified_number]=2')
+    ->assertJsonFragment(['current_page' => 2]);
