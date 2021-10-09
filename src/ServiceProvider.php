c7a792bd410c9a92cdf5bccd3a9a642e317c75a8
Brian Faust <hello@basecode.sh>
2021-10-09 09:47:56 +0300

    wip

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -4,11 +4,12 @@ declare(strict_types=1);
 
 namespace Faust\JsonApiPaginate;
 
+use Illuminate\Support\Arr;
+use Spatie\LaravelPackageTools\Package;
 use Illuminate\Database\Eloquent\Builder;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasManyThrough;
-use Spatie\LaravelPackageTools\Package;
-use Spatie\LaravelPackageTools\PackageServiceProvider;
 
 class ServiceProvider extends PackageServiceProvider
 {
@@ -19,9 +20,31 @@ class ServiceProvider extends PackageServiceProvider
             ->hasConfigFile();
     }
 
-    public function register(): void
+    public function packageRegistered(): void
     {
-        $pagination = fn (int $maxResults = null, int $defaultSize = null) => resolve(ForwardPagination::class)($this, $maxResults, $defaultSize);
+        $pagination = function (int $maxResults = null, int $defaultSize = null) {
+            $maxResults          = $maxResults ?? config('json-api-paginate.max_results');
+            $defaultSize         = $defaultSize ?? config('json-api-paginate.default_size');
+            $numberParameter     = config('json-api-paginate.number_parameter');
+            $sizeParameter       = config('json-api-paginate.size_parameter');
+            $paginationParameter = config('json-api-paginate.pagination_parameter');
+            $paginationMethod    = config('json-api-paginate.use_simple_pagination') ? 'simplePaginate' : 'paginate';
+
+            $size = (int) request()->input($paginationParameter.'.'.$sizeParameter, $defaultSize);
+
+            $size = $size > $maxResults ? $maxResults : $size;
+
+            $paginator = $this
+                ->{$paginationMethod}($size, ['*'], $paginationParameter.'.'.$numberParameter)
+                ->setPageName($paginationParameter.'['.$numberParameter.']')
+                ->appends(Arr::except(request()->input(), $paginationParameter.'.'.$numberParameter));
+
+            if (! is_null(config('json-api-paginate.base_url'))) {
+                $paginator->setPath(config('json-api-paginate.base_url'));
+            }
+
+            return $paginator;
+        };
 
         Builder::macro(config('json-api-paginate.method_name'), $pagination);
         BelongsToMany::macro(config('json-api-paginate.method_name'), $pagination);
